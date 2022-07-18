198bc0e21a05ca8fe43b17e2f0ad8a18db7947d8
Savoy D Rath <savvyz@me.com>
2022-07-18 15:40:18 -0600

    Tenzing and Savoy JWT auth

---
+++ acme_auth/db.js
@@ -0,0 +1,103 @@
+const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { STRING } = Sequelize;
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING) {
+  delete config.logging;
+}
+const conn = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  config
+);
+
+const User = conn.define('user', {
+  username: STRING,
+  password: STRING,
+});
+
+const Note = conn.define('note', {
+  text: STRING,
+});
+
+Note.belongsTo(User);
+User.hasMany(Note);
+
+User.beforeCreate(async (user) => {
+  user.password = await bcrypt.hash(user.password, 5);
+});
+
+User.byToken = async (token) => {
+  try {
+    // v JWT ADDED
+    const payload = jwt.verify(token, process.env.JWT);
+    console.log('JWT payload:', payload);
+    const user = await User.findByPk(payload.id);
+    // ^ JWT ADDED
+    if (user) {
+      return user;
+    }
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  } catch (ex) {
+    const error = Error('bad credentials');
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+    },
+  });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return jwt.sign({ id: user.id }, process.env.JWT);
+  }
+  const error = Error('bad credentials');
+  error.status = 401;
+  throw error;
+};
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: 'lucy', password: 'lucy_pw' },
+    { username: 'moe', password: 'moe_pw' },
+    { username: 'larry', password: 'larry_pw' },
+  ];
+  const notes = [
+    { text: 'hello world' },
+    { text: 'goodbye world' },
+    { text: 'hello new world' },
+  ];
+  const [note1, note2, note3] = await Promise.all(
+    notes.map((note) => Note.create(note))
+  );
+
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  await lucy.setNotes(note1);
+  await moe.setNotes([note2, note3]);
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
+  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+    Note,
+  },
+};
